97512440ce5cae393f1df162abb5c8c483527c1a
Apple12345123 <pgh3621@naver.som>
2024-02-23 16:33:25 +0900

    Test1

---
+++ src/java_test/Item.java
@@ -0,0 +1,19 @@
+package java_test;
+
+public class Item {
+    private String name;
+    private long price;
+    private int quantity;
+
+    public Item(String name, long price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+    public String getName() {
+        return name;
+    }
+    public long getTotal() {
+        return price * quantity;
+    }
+}
---
+++ src/java_test/Test8.java
@@ -0,0 +1,13 @@
+package java_test;
+
+import static java_test.MathArrayUtil.*;
+
+public class Test8 {
+    public static void main(String[] args) {
+        int[] values = {1, 2, 3, 4, 5, 10, 20, 30};
+        System.out.println("sum=" + sum(values));
+        System.out.println("average=" + average(values));
+        System.out.println("min=" + min(values));
+        System.out.println("max=" + max(values));
+    }
+}
